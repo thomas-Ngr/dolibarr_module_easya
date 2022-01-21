5e76fde708115030b7c170afc5f0d45d6f7e67a6
Alexandre SPANGARO <aspangaro.dolibarr@gmail.com>
2022-01-21 05:06:02 +0100

    Activation par défaut du style d'icone FA6 light

--- core/modules/modeasya.class.php
+++ core/modules/modeasya.class.php
@@ -156,6 +156,25 @@ class modeasya extends DolibarrModules
         $this->const [$r] [5] = 'all';
         $this->const [$r] [6] = 1;
 
+        // Activate icon style Font awesome 6 light by default
+        $r ++;
+        $this->const [$r] [0] = "MAIN_FONTAWESOME_ICON_STYLE";
+        $this->const [$r] [1] = "chaine";
+        $this->const [$r] [2] = "fal";
+        $this->const [$r] [3] = 0;
+        $this->const [$r] [4] = 1;
+        $this->const [$r] [5] = 'all';
+        $this->const [$r] [6] = 1;
+
+        $r ++;
+        $this->const [$r] [0] = "MAIN_FONTAWESOME_WEIGHT";
+        $this->const [$r] [1] = "chaine";
+        $this->const [$r] [2] = "300";
+        $this->const [$r] [3] = 0;
+        $this->const [$r] [4] = 1;
+        $this->const [$r] [5] = 'all';
+        $this->const [$r] [6] = 1;
+
 		// Array to add new pages in new tabs
 		// Example: $this->tabs = array('objecttype:+tabname1:Title1:mylangfile@mymodule:$user->rights->mymodule->read:/mymodule/mynewtab1.php?id=__ID__',  					// To add a new tab identified by code tabname1
         //                              'objecttype:+tabname2:SUBSTITUTION_Title2:mylangfile@mymodule:$user->rights->othermodule->read:/mymodule/mynewtab2.php?id=__ID__',  	// To add another new tab identified by code tabname2. Label will be result of calling all substitution functions on 'Title2' key.
